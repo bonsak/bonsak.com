273e9c95066469a6d55ad0d9f1fda2f6b865a115
Bonsak Schiledrop <1322824+bonsak@users.noreply.github.com>
2026-04-06 01:47:08 +0200

    fix: full url for hero image

---
+++ pb_hooks/main.pb.js
@@ -0,0 +1,17 @@
+// pb_hooks/main.pb.js
+onRecordEnrich((e) => {
+  const record = e.record;
+  const filename = record.get("hero_image");
+
+  if (filename) {
+    const appUrl = $app.settings().meta.appUrl || "http://127.0.0.1:8090";
+    const collectionId = record.collection().id;
+    const recordId = record.id;
+    const fullUrl = `${appUrl}/api/files/${collectionId}/${recordId}/${filename}`;
+
+    record.withCustomData(true);
+    record.set("hero_image_url", fullUrl);
+  }
+
+  e.next();
+}, "work");
