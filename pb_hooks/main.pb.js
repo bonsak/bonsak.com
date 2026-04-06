d421c582eeb0731cc078e18871228fddee8bb003
Bonsak Schiledrop <1322824+bonsak@users.noreply.github.com>
2026-04-06 15:49:42 +0200

    feat: add full urls for more_images

--- pb_hooks/main.pb.js
+++ pb_hooks/main.pb.js
@@ -1,16 +1,29 @@
 // pb_hooks/main.pb.js
 onRecordEnrich((e) => {
   const record = e.record;
-  const filename = record.get("hero_image");
+  const appUrl = $app.settings().meta.appURL || "http://127.0.0.1:8090";
+  const collectionId = record.collection().id;
+  const recordId = record.id;
 
-  if (filename) {
-    const appUrl = $app.settings().meta.appURL || "http://127.0.0.1:8090";
-    const collectionId = record.collection().id;
-    const recordId = record.id;
-    const fullUrl = `${appUrl}/api/files/${collectionId}/${recordId}/${filename}`;
+  record.withCustomData(true);
 
-    record.withCustomData(true);
-    record.set("hero_image_url", fullUrl);
+  // Hero image URL
+  const heroImage = record.get("hero_image");
+  if (heroImage) {
+    record.set(
+      "hero_image_url",
+      `${appUrl}/api/files/${collectionId}/${recordId}/${heroImage}`
+    );
+  }
+
+  // More images URLs
+  const moreImages = record.get("more_images");
+  if (moreImages && moreImages.length > 0) {
+    const moreImagesUrls = moreImages.map(
+      (filename) =>
+        `${appUrl}/api/files/${collectionId}/${recordId}/${filename}`
+    );
+    record.set("more_images_urls", moreImagesUrls);
   }
 
   e.next();
